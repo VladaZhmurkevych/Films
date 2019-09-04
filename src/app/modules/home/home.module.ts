b59e05ab27b51d247c6807a26353a773aee9121d
Vladyslava Zhmurkevych <vladyslava_zhmurkevych@epam.com>
2019-09-04 16:52:59 +0300

    add type of film api

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilmComponent } from './film/film.component';
+
+
+
+@NgModule({
+  declarations: [FilmComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
